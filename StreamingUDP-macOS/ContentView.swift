35b4472a1fce9d87b89bcb78eea56027dff2e41b
khoanisdev <doanvankhoan124@gmail.com>
2025-02-22 18:48:35 +0700

    Initial Commit

---
+++ StreamingUDP-macOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreamingUDP-macOS
+//
+//  Created by Đoàn Văn Khoan on 22/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreamingUDP-macOS/StreamingUDP_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingUDP_macOSApp.swift
+//  StreamingUDP-macOS
+//
+//  Created by Đoàn Văn Khoan on 22/2/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamingUDP_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
